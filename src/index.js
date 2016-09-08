8d7e5bf9cc0768725585d99e2cb8718d76bcc41f
bdadam <me@bdadam.com>
2016-09-08 14:33:49 +0200

    some structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // ES2015
 // - const, let
 // - arrow functions + this binding
+// - default parameters
 // - template literals
 // - modules
 // - destructuring
@@ -11,49 +12,10 @@
 // - Array.includes
 // - exponential operator
 
-const game = {
-    clickFactor: 1,
-    score: 0,
-    inventory: {
-        autoClicker: 0,
-        grandma: 0,
-        factory: 0
-    },
-    prices: {
-        autoClicker: 10,
-        grandma: 50,
-        factory: 1000
-    },
+import game from './game';
 
-    click() {
-        this.score += this.clickFactor;
-    },
-
-    update() {
-
-    },
-
-    showStats() {
-        document.querySelector('#stats').innerHTML = `
-            <p>Score: ${this.score}</p>
-            <p>Autoclickers: ${this.inventory.autoClicker}</p>
-            <p>Grandmas @ work: ${this.inventory.grandma}</p>
-            <p>Factories: ${this.inventory.factory}</p>
-        `;
-    }
-};
-
-game.showStats();
-
-const btnCookie = document.querySelector('#btn-cookie');
-
-btnCookie.addEventListener('click', e => {
-    game.click();
-    game.showStats();
-    // console.log(game.score);
-});
+game.update();
 
 setInterval(() => {
     game.update();
-    game.showStats();
 }, 1000);
